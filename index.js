43e23a4f1bbcffac1ead5831099d0dd8ea1fbeea
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2021-06-09 00:05:04 +0200

    nowebpack

---
+++ index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import 'css/styles.css'
+
+
+function component() {
+  const element = document.createElement('div');
+
+	  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
